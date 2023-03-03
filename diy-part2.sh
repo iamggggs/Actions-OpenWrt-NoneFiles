dcaa5e912fe9960e3b3390a31b06c31ff6615d4e
iamggggs <86513842+iamggggs@users.noreply.github.com>
2023-03-03 22:33:51 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,13 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.0.1/g' package/base-files/files/bin/config_generate
+
+# Modify luci-app-adbyby-plus default update time
+# sed -i 's/0 6/0 8/g' package/lean/luci-app-adbyby-plus/root/etc/init.d/adbyby
+
+# Add luci-app-ssr-plus
+mkdir package/base-files/files/config
+echo 0xDEADBEEF > package/base-files/files/config/google_fu_mode
+echo "src-git helloworld https://github.com/fw876/helloworld" >> feeds.conf.default
+./scripts/feeds update -a && ./scripts/feeds install -a
